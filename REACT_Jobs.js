4644961da7568bd04c086b63aa14d6d165c95cff
Tink <85289337+tink012184@users.noreply.github.com>
2023-10-27 15:53:23 -0600

    REACT change

--- REACT_Jobs.js
+++ REACT_Jobs.js
@@ -54,7 +54,7 @@ function CreateNoteEmail() {
     var SSlink = document.getElementById("SS-link").value;
 
     
-    let notes = "\nAlarm Count: " + alarmcount + "\nLHWO count: " + LHWocount + "\nIVR Calls: " + IVRcount + "\nTC Count: " + TCcount + "\nTC Per Account: " + TCpercount + "\nCombined Activity Score: " + activity + "\nLink to Lighthouse work order: " + LHlink + "\nLink to Smartsheet: " + SSlink + "\n\nROC Triage: \n\nRecent MT Work: \n\n" + "\n\n" + allnotes + "\"Screenshots to support the ROC Triage\"";
+    let notes = "\nAlarm Count: " + alarmcount + "\nLHWO count: " + LHWocount + "\nIVR Calls: " + IVRcount + "\nTC Count: " + TCcount + "\nTC Per Account: " + TCpercount + "\nCombined Activity Score: " + activity + "\nLink to Lighthouse work order: " + LHlink + "\nLink to Smartsheet: " + SSlink + "\n\nROC Triage: \n\n" + "\n\n" + allnotes + "\"Screenshots to support the ROC Triage\"";
 
  return notes;
 }
@@ -135,7 +135,7 @@ function generate(){
 
     let mailTo = "";
 	 
-	mailTo = "mailto:" + management + TechEmail + "?subject=" + MA + " - High Demand/REACT Node - Hub: " + hub + " - Node: " + node + "&body=" + encodeURIComponent("This email is to alert you to a node driving high activity in your area. Please be sure to troubleshoot thoroughly and take any necessary actions to ensure resolution to the ongoing issues. Once the job is resolved, please have the MT respond to the email with the resolution. Thank you.") + encodeURIComponent(CreateNoteEmail());
+	mailTo = "mailto:" + management + TechEmail + "?subject=" + MA + " - High Demand/REACT Node - Hub: " + hub + " - Node: " + node + "&body=" + encodeURIComponent("This email is to alert you to a node driving high activity in your area. This node will need to be certified/optimized/swept. Once node certification/optimization/sweeping has been completed, the ROC will provide targets, as needed. 1 hour of DM time per day outside of the maintenance window is permitted. DM work during the maintenance window on REACT jobs is BAU. Please utilize the “ART REACT” Webex chat for the enabling/disabling of deflection outside of maintenance window. Impairment Deflection will need to be enabled when this job is being worked (EN ROUTE/WIP). Impairment Deflection does not allow for plant to be taken down. Its purpose is to notify customers that there is a technician working in their area. All updates need to be added to this thread daily. This includes what was swept, what has been replaced, what issues have been found, what improvements have been seen, where we left off, what the next steps are, etc. Please feel free to reach out to the ROC 4 line if you need any assistance or have any questions or concerns. Thank you. ") + encodeURIComponent(CreateNoteEmail());
 
     document.location.href = mailTo;
 }
